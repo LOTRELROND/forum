47e1bb55b1b5271b44b668ca5763649922e31aed
LOTRELROND <mehmetkemalkayam@gmail.com>
2022-06-15 13:59:45 +0300

    The first time

---
+++ event/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('detail/<slug>/', views.detail, name='detail'),
+    path('addentry/', views.addEntry, name='addentry'),
+    path('addauthor/', views.addAuthor, name = 'addauthor' ),
+    path('delete/<slug>/', views.deletePost, name = 'deletepost' ),
+    path('dashboard', views.dashboard, name = 'dashboard' ),
+    path('edit/<slug>/', views.editEntry, name = 'edit' ),
+    path('profile/<slug>',views.profile, name = 'profile'),
+    #path('addreply', views.addReply, name='addreply'),
+    #path('updateauthor/', views.upAuthor, name = 'updateauthor' ),
+]
